f13b3df427c50020d28414583d1006ad144ca33d
anastasiiaboiko <anastacia.boiko@gmail.com>
2015-01-26 15:54:52 -0800

    Implemented drawRect
    …to draw the current and finished lines.

--- MyTouchTracker/BNRDrawView.m
+++ MyTouchTracker/BNRDrawView.m
@@ -7,8 +7,54 @@
 //
 
 #import "BNRDrawView.h"
+#import "BNRLine.h"
+
+@interface BNRDrawView ()
+
+@property (nonatomic, strong) BNRLine *currentLine;
+@property (nonatomic, strong) NSMutableArray *finishedLines;
+
+@end
 
 @implementation BNRDrawView
+
+- (instancetype)initWithFrame:(CGRect)r
+{
+    self = [super initWithFrame:r];
+    
+    if (self) {
+        self.finishedLines = [[NSMutableArray alloc] init];
+        self.backgroundColor = [UIColor grayColor];
+    }
+    
+    return self;
+}
+
+- (void)strokeLine:(BNRLine *)line
+{
+    UIBezierPath *bp = [UIBezierPath bezierPath];
+    bp.lineWidth = 10;
+    bp.lineCapStyle = kCGLineCapRound;
+    
+    [bp moveToPoint:line.begin];
+    [bp addLineToPoint:line.end];
+    [bp stroke];
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    // Draw finished lines in black
+    [[UIColor blackColor] set];
+    for (BNRLine *line in self.finishedLines) {
+        [self strokeLine:line];
+    }
+    
+    if (self.currentLine) {
+        // If there is a line currently being drawn, do it in red
+        [[UIColor redColor] set];
+        [self strokeLine:self.currentLine];
+    }
+}
 
 /*
 // Only override drawRect: if you perform custom drawing.
