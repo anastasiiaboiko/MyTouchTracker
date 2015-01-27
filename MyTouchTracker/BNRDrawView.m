035fe5331683f3b86cf85631455493c136dc7432
anastasiiaboiko <anastacia.boiko@gmail.com>
2015-01-26 16:28:12 -0800

    Turning touches into lines & handling multiple...
    …touches.
    
    - implemented touchesBegan:withEvent: to create a new line;
    - implemented touchesMoved:withEvent: that updates the end of the
    currentLine;
    - added the currentLine to the finishedLines when the touch ends;
    - enabled BNRDrawView instances to accept multiple touches.
    
    Then:
    - updated the UIResponder methods to add lines that are currently being
    drawn to this dictionary;
    - updated touchesMoved:withEvent: in BNRDrawView.m so that it can look
    up the right BNRLine;
    - updated touchesEnded:withEvent: to move any finished lines into the
    _finishedLines array;
    - updated drawRect: to draw each line in _linesInProgress;
    - implemented touchesCancelled:withEvent.

--- MyTouchTracker/BNRDrawView.m
+++ MyTouchTracker/BNRDrawView.m
@@ -11,7 +11,7 @@
 
 @interface BNRDrawView ()
 
-@property (nonatomic, strong) BNRLine *currentLine;
+@property (nonatomic, strong) NSMutableDictionary *linesInProgress;
 @property (nonatomic, strong) NSMutableArray *finishedLines;
 
 @end
@@ -23,8 +23,10 @@
     self = [super initWithFrame:r];
     
     if (self) {
+        self.linesInProgress = [[NSMutableDictionary alloc] init];
         self.finishedLines = [[NSMutableArray alloc] init];
         self.backgroundColor = [UIColor grayColor];
+        self.multipleTouchEnabled = YES;
     }
     
     return self;
@@ -47,13 +49,78 @@
     [[UIColor blackColor] set];
     for (BNRLine *line in self.finishedLines) {
         [self strokeLine:line];
+        [[UIColor redColor] set];
+        for (NSValue *key in self.linesInProgress) {
+            [self strokeLine:self.linesInProgress[key]];
+        }
+    }
+}
+
+- (void)touchesBegan:(NSSet *)touches
+            withEvent:(UIEvent *)event
+{
+    // Let's put in a log statement to see the order of events
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    
+    for (UITouch *t in touches) {
+        CGPoint location = [t locationInView:self];
+        
+        BNRLine *line = [[BNRLine alloc] init];
+        line.begin = location;
+        line.end = location;
+        
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        self.linesInProgress[key] = line;
     }
     
-    if (self.currentLine) {
-        // If there is a line currently being drawn, do it in red
-        [[UIColor redColor] set];
-        [self strokeLine:self.currentLine];
+    [self setNeedsDisplay];
+}
+
+- (void)touchesMoved:(NSSet *)touches
+            withEvent:(UIEvent *)event
+{
+    // Let's put in a log statement to see the order of events
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    
+    for (UITouch *t in touches) {
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        BNRLine *line = self.linesInProgress[key];
+        
+        line.end = [t locationInView:self];
     }
+    
+    [self setNeedsDisplay];
+}
+
+- (void)touchesEnded:(NSSet *)touches
+            withEvent:(UIEvent *)event
+{
+    // Let's put in a log statement to see the order of events
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    
+    for (UITouch *t in touches) {
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        BNRLine *line = self.linesInProgress[key];
+        
+        [self.finishedLines addObject:line];
+        [self.linesInProgress removeObjectForKey:key];
+    }
+    
+    [self setNeedsDisplay];
+}
+
+- (void)touchesCancelled:(NSSet *)touches
+                withEvent:(UIEvent *)event
+{
+    // Let's put in a log statement to see the order of events
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    
+    for (UITouch *t in touches) {
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        [self.linesInProgress removeObjectForKey:key];
+    }
+    
+    [self setNeedsDisplay];
 }
 
 /*
--- MyTouchTracker/BNRLine.h
+++ MyTouchTracker/BNRLine.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CGGeometry.h>
 
 @interface BNRLine : NSObject
 
